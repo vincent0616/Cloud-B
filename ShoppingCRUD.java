9ba0aad589f414d50f09c467526f36d19808922e
vincent0616 <150211225+vincent0616@users.noreply.github.com>
2023-12-05 16:46:23 +0900

    Add files via upload

---
+++ ShoppingCRUD.java
@@ -0,0 +1,22 @@
+package jdbc;
+
+/*  1. member_table
+	새로운 회원을 추가(INSERT) - 모든 항목
+	
+	2. 제품테이블(product_table)
+	- 데일리 푸퍼 숏 패딩 재킷(3번)
+	조회해서 CONSOLE 출력
+	
+	3. 카테고리 테이블(cate_table)
+	- 아우터 -> 숏패딩
+	
+	4. 주문 테이블(order_table)
+	- id값이 2인 항목을 삭제,
+	주문 테이블 콘솔 출력 
+	SET foreign_key_checks = 0; 
+ */
+public class ShoppingCRUD {
+	public static void main(String[] args) {
+		
+	}
+}
